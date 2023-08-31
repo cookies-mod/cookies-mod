8ff3bba73d857837c4ccd34ed95a3d4d4f32064e
Morazzer <morazzer@morazzer.dev>
2023-09-01 01:38:29 +0200

    feat: add action to create changelogs and releases on push

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 //file:noinspection GroovyUnusedAssignment
 plugins {
+    id("org.ajoberstar.grgit") version "5.2.0" apply false
     id("fabric-loom") version "1.3-SNAPSHOT"
     id("maven-publish")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     kotlin("jvm") version "1.9.0"
 }
 
@@ -13,14 +15,32 @@ base {
 }
 
 repositories {
-    maven {
-        url = uri("https://maven.ladysnake.org/releases")
-    }
+    maven { url = uri("https://maven.ladysnake.org/releases") }
     maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://maven.wispforest.io") }
+    maven { url = uri("https://repo.morazzer.dev/releases") }
+}
 
-    maven {
-        url = uri("https://maven.wispforest.io")
-    }
+val shadowImplementation: Configuration by configurations.creating {
+    configurations.implementation.get().extendsFrom(this)
+}
+
+val shadowRuntimeOnly: Configuration by configurations.creating {
+    configurations.runtimeOnly.get().extendsFrom(this)
+}
+
+val shadowFinalMod: Configuration by configurations.creating {
+}
+
+val shadowModImplementation: Configuration by configurations.creating {
+    configurations.modImplementation.get().extendsFrom(this)
+}
+
+val shadowModApi: Configuration by configurations.creating {
+    configurations.modApi.get().extendsFrom(this)
+}
+val shadowModCompileOnly: Configuration by configurations.creating {
+    configurations.modCompileOnly.get().extendsFrom(this)
 }
 
 dependencies {
@@ -30,10 +50,17 @@ dependencies {
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.properties["fabric_version"]}")
     modImplementation("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"]}")
-    include(modImplementation("com.github.0x3C50:Renderer:master-SNAPSHOT")!!)
+    modImplementation("com.github.0x3C50:Renderer:master-SNAPSHOT")
 
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("org.reflections:reflections:0.10.2")
+    shadowImplementation("org.reflections:reflections:0.10.2") {
+        exclude(module = "slf4j-api")
+    }
+
+    shadowFinalMod("dev.morazzer:moulconfig:1.0.0-fix+1") {
+        isTransitive = false
+    }
+    implementation("dev.morazzer:moulconfig:1.0.0-fix+1:dev")
 
     annotationProcessor("org.projectlombok:lombok:1.18.26")
     compileOnly("org.projectlombok:lombok:1.18.26")
@@ -79,3 +106,15 @@ publishing {
 loom {
     accessWidenerPath.set(file("src/main/resources/cookiesmod.accesswidener"))
 }
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    configurations = listOf(shadowImplementation, shadowModImplementation, shadowModApi, shadowModCompileOnly, shadowFinalMod, shadowRuntimeOnly)
+
+    relocate("io.github.moulberry", "dev.morazzer")
+    archiveVersion.set("v${rootProject.version.toString().lowercase()}")
+    archiveClassifier.set("")
+}
+
+apply(from = "gradle/generate-changelog.gradle")
+apply(from = "gradle/create-tag-name.gradle")
+apply(from = "gradle/hash-libraries.gradle")
