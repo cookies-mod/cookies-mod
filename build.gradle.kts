f75a6ad406e00e807e6d1d7e37659375bb940290
Morazzer <morazzer@morazzer.dev>
2023-09-01 14:15:27 +0200

    Pre release v.0.0.1 (#3)
    * feat: auto download repo in case its missing
    
    * fix: make fatjar remapped to work with normal minecraft
    
    * fix: change hash function to only run when called
    
    * feat: remove kotlin since it's not useful for the scope of this mod
    
    * fix: remove unnecessary lines

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import net.fabricmc.loom.task.RemapJarTask
+
 //file:noinspection GroovyUnusedAssignment
 plugins {
     id("org.ajoberstar.grgit") version "5.2.0" apply false
     id("fabric-loom") version "1.3-SNAPSHOT"
     id("maven-publish")
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    kotlin("jvm") version "1.9.0"
+    //kotlin("jvm") version "1.9.0"
 }
 
 version = project.properties["mod_version"]!!
@@ -21,26 +24,9 @@ repositories {
     maven { url = uri("https://repo.morazzer.dev/releases") }
 }
 
-val shadowImplementation: Configuration by configurations.creating {
+val shadowConfiguration: Configuration by configurations.creating {
+    this.isCanBeResolved = true
     configurations.implementation.get().extendsFrom(this)
-}
-
-val shadowRuntimeOnly: Configuration by configurations.creating {
-    configurations.runtimeOnly.get().extendsFrom(this)
-}
-
-val shadowFinalMod: Configuration by configurations.creating {
-}
-
-val shadowModImplementation: Configuration by configurations.creating {
-    configurations.modImplementation.get().extendsFrom(this)
-}
-
-val shadowModApi: Configuration by configurations.creating {
-    configurations.modApi.get().extendsFrom(this)
-}
-val shadowModCompileOnly: Configuration by configurations.creating {
-    configurations.modCompileOnly.get().extendsFrom(this)
 }
 
 dependencies {
@@ -49,18 +35,16 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:${project.properties["loader_version"]}")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.properties["fabric_version"]}")
-    modImplementation("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"]}")
-    modImplementation("com.github.0x3C50:Renderer:master-SNAPSHOT")
+    //include(modImplementation("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"]}")!!)
+    include(modImplementation("com.github.0x3C50:Renderer:master-SNAPSHOT")!!)
 
-    implementation("com.google.code.gson:gson:2.10.1")
-    shadowImplementation("org.reflections:reflections:0.10.2") {
+    implementation("com.google.code.gson:gson:2.10.1")!!
+    shadowConfiguration(implementation("org.reflections:reflections:0.10.2") {
         exclude(module = "slf4j-api")
-    }
+    })
+    shadowConfiguration(implementation("org.eclipse.jgit:org.eclipse.jgit:3.5.0.201409260305-r")!!)
 
-    shadowFinalMod("dev.morazzer:moulconfig:1.0.0-fix+1") {
-        isTransitive = false
-    }
-    implementation("dev.morazzer:moulconfig:1.0.0-fix+1:dev")
+    include(modImplementation("dev.morazzer:moulconfig:1.0.0-fix+2")!!)
 
     annotationProcessor("org.projectlombok:lombok:1.18.26")
     compileOnly("org.projectlombok:lombok:1.18.26")
@@ -76,8 +60,16 @@ tasks.withType<JavaCompile>().configureEach {
     this.options.release.set(17)
 }
 
-kotlin {
-    jvmToolchain(17)
+tasks.getByName<ShadowJar>("shadowJar") {
+    archiveClassifier.set("dev")
+    configurations = listOf(shadowConfiguration)
+}
+
+tasks.getByName<RemapJarTask>("remapJar") {
+    this.dependsOn(tasks.getByName<ShadowJar>("shadowJar"))
+    println(tasks.getByName<ShadowJar>("shadowJar").archiveFile)
+    inputFile = tasks.getByName<ShadowJar>("shadowJar").archiveFile
+    archiveClassifier = "remapped"
 }
 
 java {
@@ -105,14 +97,6 @@ publishing {
 
 loom {
     accessWidenerPath.set(file("src/main/resources/cookiesmod.accesswidener"))
-}
-
-tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
-    configurations = listOf(shadowImplementation, shadowModImplementation, shadowModApi, shadowModCompileOnly, shadowFinalMod, shadowRuntimeOnly)
-
-    relocate("io.github.moulberry", "dev.morazzer")
-    archiveVersion.set("v${rootProject.version.toString().lowercase()}")
-    archiveClassifier.set("")
 }
 
 apply(from = "gradle/generate-changelog.gradle")
