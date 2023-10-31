b724b57d0f6a6bf93dc62cb6ab79b78087faf18e
morazzer <morazzer@morazzer.dev>
2023-10-31 20:38:05 +0100

    feat: fix wrong java doc
    Signed-off-by: morazzer <morazzer@morazzer.dev>

--- src/main/java/dev/morazzer/cookiesmod/data/migrations/Migration.java
+++ src/main/java/dev/morazzer/cookiesmod/data/migrations/Migration.java
@@ -9,13 +9,15 @@ public interface Migration<T> {
 
     /**
      * Gets the unique number of the migration.
+     * This number incrementally counts up with each migration
+     * and is used to track whether a migration is already applied or not.
      *
      * @return The number.
      */
     long getNumber();
 
     /**
-     * Applies the migration to the data. This number incrementally counts up with each migration and is used to track whether a migration is already applied or not.
+     * Applies the migration to the data.
      *
      * @param value The data to apply it to.
      */
